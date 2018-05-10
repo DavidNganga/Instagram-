3caa09c493635c378536f93628fa3a7635f8c77e
DavidNganga <ngashiedavid@gmail.com>
2018-05-10 11:31:03 +0300

    created project folder

---
+++ share/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=30,null = True)
+    bio = models.CharField(max_length = 30)
+    profile_photo = models.ImageField(upload_to = 'images/',null = True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null = True)
+
+    def __str__(self):
+        return self.name
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    
+
+class Image(models.Model):
+    name = models.CharField(max_length =30)
+    caption = models.CharField(max_length = 100)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE,null=True)
+    image = models.ImageField(upload_to = 'images/',null = True)
+
+    def __str__(self):
+        return self.name
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
+
+    def get_Image_by_id(cls,id):
+        images = cls.objects.get(id=id)
+        return images
